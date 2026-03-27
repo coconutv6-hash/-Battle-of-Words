aafa7b9646e070ef6f49fae3b5ec2c00eaa3bec3
Kacper Domżalski <kacperdomzalski@laptop-Kacper.local>
2026-03-27 19:33:33 +0100

    Deploy web build with latest UI changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"eb0b756f6e05e1d0e71ae2dc2cd84d4cc4fea75f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2641617181" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3862307215" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
